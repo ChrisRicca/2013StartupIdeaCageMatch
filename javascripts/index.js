ec1eb1c9da015450ebbcd641a2c58a6a5091dad4
Chris Ricca <christopher.ricca@gmail.com>
2013-11-18 10:53:21 -0500

    Automated commit at 2013-11-18 15:53:21 UTC by middleman-deploy 0.1.3

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,9 @@
-throw Error("Interrupt: \n  (in /Users/chris/Development/social/2013StartupIdeaCageMatch/source/javascripts/index.js.coffee)")
+(function() {
+
+  $('.show-vote').on('click', function(e) {
+    $(this).hide();
+    $(this).siblings('.vote-container').slideDown();
+    return e.preventDefault();
+  });
+
+}).call(this);
